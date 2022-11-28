c5fcf5fd3cdfae8a1a769522f1b2226e59122ebd
Kenni Woodard <kennikristine@gmail.com>
2022-11-28 14:30:04 -0800

    todos appear on load for user

--- todos/todos.js
+++ todos/todos.js
@@ -54,6 +54,9 @@ async function displayTodos() {
 }
 
 // add page load function
+window.addEventListener('load', async () => {
+    displayTodos();
+});
 // fetch the todos and store in state
 // call displayTodos
 
